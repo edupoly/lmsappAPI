de3683da446f0d992f0038445f297cd68557cbb6
dasari987 <dasarisaibabu987@gmail.com>
2024-08-10 15:33:22 +0530

    first

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+
+var app = express()
+app.use(express.static(__dirname+"/public"))
+
+
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+
+app.listen(8090,()=>{
+    console.log('server is running on 8090')
+})

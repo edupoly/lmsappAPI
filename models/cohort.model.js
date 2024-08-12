1473d6600139a584941d78c5584e1b0762248f14
dasari987 <dasarisaibabu987@gmail.com>
2024-08-12 15:25:25 +0530

    dateAdded

--- models/cohort.model.js
+++ models/cohort.model.js
@@ -5,8 +5,10 @@ const cohortSchema = new Schema({
     cohortname: { type: String, required: true },
     cohortid: { type: String, required: true },
     cohorttags: { type: [String], required: true },  // Array of strings for tags
-    cohortpic: { type: String, required: true }  // Path to the uploaded cohort picture
+    cohortpic: { type: String, required: true },  // Path to the uploaded cohort picture
+    cohortdate: { type: String, required: true }  // Cohort date as a string
 });
 
 const Cohort = mongoose.model('Cohort', cohortSchema);
 module.exports = Cohort;
+
